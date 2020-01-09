94ed525993e8f9158130f6bf8ab134a3c709e113
amartya <amartya.sk@gmail.com>
2020-01-09 18:30:54 +0700

    temp

---
+++ _site/README.txt
@@ -0,0 +1,21 @@
+How to Setup Development Setup Environment:
+
+requirement:
+- Node.js and npm
+to check, run: 
+node -v
+this will return ur current version of node
+
+run these commands:
+sudo npm install gulp --global
+npm install 
+
+* gulp and browser-sync are for development only
+
+* to run dev server type 'gulp' in the terminal and open localhost:6969
+
+then ur good to go; the dev server will auto reload every html or css changes :D
+
+Happy Stylesheeting!
+
+i hacked you! email me at marcantoniosmap@gmail.com
---
+++ test.js
@@ -0,0 +1,30 @@
+// source: https://coursetro.com/posts/code/130/Learn-Bootstrap-4-Final-in-2018-with-our-Free-Crash-Course
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+    return gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js', 
+        'node_modules/jquery/dist/jquery.min.js', 
+        'node_modules/popper.js/dist/umd/popper.min.js'
+    ])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+});
+
+// Static Server + watching css/html files
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: ".",
+        port: 6969
+    });
+
+    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/css/*.css").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('js','serve'));
